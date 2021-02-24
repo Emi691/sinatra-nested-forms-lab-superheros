ee3c5830aad10d9e26bcacd472c2872f53611843
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-02-23 16:34:04 -0800

    commit tree 45ac5b10d8a705a0f3b18150b1105a6ec294addc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get '/' do
+       erb  :super_hero
+    end
 
+    post '/teams' do
+        @team = Team.new(params[:team])
+        params[:team][:hero].each do |member|
+            Hero.new(member)
+        end
+        @members = Hero.all
+        erb :team
+    end
 
 end
